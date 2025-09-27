1e42dcd048a9e1616c76696f5677147647f9933b
stuti027 <stutishiv18@gmail.com>
2025-09-27 14:58:23 +0530

    login signup

--- app/src/main/java/com/example/remedi/Login.java
+++ app/src/main/java/com/example/remedi/Login.java
@@ -1,14 +1,33 @@
 package com.example.remedi;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class Login extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+    private EditText logEmail, logPass;
+    private TextView signupRedirect;
+    private Button logButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +38,47 @@ public class Login extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        auth=FirebaseAuth.getInstance();
+        logEmail = findViewById(R.id.logEmail);
+        logPass = findViewById(R.id.logPass);
+        logButton = findViewById(R.id.logButton);
+        signupRedirect = findViewById(R.id.signupRedirect);
+        logButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = logEmail.getText().toString();
+                String pass = logPass.getText().toString();
+                if (email.isEmpty() || Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    if (!pass.isEmpty()) {
+                        auth.signInWithEmailAndPassword(email, pass)
+                                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                                    @Override
+                                    public void onSuccess(AuthResult authResult) {
+                                        Toast.makeText(Login.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(Login.this, HomePage.class));
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(Login.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                    } else {
+                        logPass.setError("Please enter password");
+                    }
+                } else if(email.isEmpty()) {
+                    logEmail.setError("Email cannot be empty");
+                } else {
+                    logEmail.setError("Please enter valid email");
+                }
+            }
+        });
+        signupRedirect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Login.this, SignUp.class));
+            }
         });
     }
 }
--- app/src/main/java/com/example/remedi/SignUp.java
+++ app/src/main/java/com/example/remedi/SignUp.java
@@ -1,14 +1,39 @@
 package com.example.remedi;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class SignUp extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+    private FirebaseFirestore db;
+
+    private EditText signEmail, signPass;
+    private Button signButton;
+    private TextView loginRedirect;
+    private String userRole;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +44,59 @@ public class SignUp extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        userRole = getIntent().getStringExtra("role");
+        if (userRole == null) {
+            userRole = "patient";
+        }
+        auth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+        signEmail = findViewById(R.id.signEmail);
+        signPass = findViewById(R.id.signPass);
+        signButton = findViewById(R.id.signButton);
+        loginRedirect = findViewById(R.id.loginRedirect);
+        signButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = signEmail.getText().toString().trim();
+                String pass = signPass.getText().toString().trim();
+                if(user.isEmpty()) {
+                    signEmail.setError("Email cannot be empty");
+                }
+                if(pass.isEmpty()) {
+                    signPass.setError("Please enter password");
+                }
+                else {
+                    auth.createUserWithEmailAndPassword(user, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                String uid = auth.getCurrentUser().getUid();
+                                Map<String, Object> userData = new HashMap<>();
+                                userData.put("email", user);
+                                userData.put("type", "patient");
+                                userData.put("guardianId", null);
+                                db.collection("users").document(uid)
+                                        .set(userData)
+                                        .addOnSuccessListener(aVoid -> {
+                                            Toast.makeText(SignUp.this, "Sign up successful", Toast.LENGTH_SHORT).show();
+                                            startActivity(new Intent(SignUp.this, HomePage.class));
+                                        })
+                                        .addOnFailureListener(e -> {
+                                            Toast.makeText(SignUp.this, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                                        });
+                            }
+
+                        }
+                    });
+                }
+            }
+        });
+        loginRedirect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SignUp.this, Login.class));
+            }
         });
     }
 }
