6e51db8a2236d19c341037a1ffb9813c1a343cbf
stuti027 <stutishiv18@gmail.com>
2025-09-27 16:58:09 +0530

    login signup

--- app/src/main/java/com/example/remedi/ChooseMode.java
+++ app/src/main/java/com/example/remedi/ChooseMode.java
@@ -1,6 +1,8 @@
 package com.example.remedi;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,11 @@ public class ChooseMode extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void PatientMode(View view) {
+        startActivity(new Intent(ChooseMode.this, SignUp.class));
+    }
+    public void GuardianMode(View view) {
+        startActivity(new Intent(ChooseMode.this, LinkPatient.class));
     }
 }
